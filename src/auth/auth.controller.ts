b52944043f03d4452c3699c4c9d7e0cfc118e955
6ill <kyrbill123@gmail.com>
2025-04-15 14:40:51 +0700

    feat/auth/add endpoint to check JWT token expiration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import { Body, Controller, Get, Post, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDto, RegisterUserDto } from './dtos';
+import { JwtGuard } from './guard/jwt.guard';
+import { User } from 'src/common/decorators';
+import { UserSession } from 'src/common/interfaces';
 
 @Controller('auth')
 export class AuthController {
@@ -20,4 +23,13 @@ export class AuthController {
 
         return this.authService.signIn(user);
     }
+
+    @Get('check')
+    @UseGuards(JwtGuard)
+    async check(@User() user: UserSession) {
+        return {
+            user
+        }
+    }
+    
 }
